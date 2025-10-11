b2384067aa4b51479083dcdc0b8aa6c5be9bca7d
Amadeusz Kosik <git.stunning853@passmail.com>
2025-10-11 23:33:56 +0200

    Add solution: 0203.

---
+++ 0203-remove-linked-list-elements.c
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    if(head == NULL) {
+        return NULL;
+    } else if(head -> val == val) {
+        return removeElements(head -> next, val);
+    } else {
+        head -> next = removeElements(head -> next, val);
+        return head;
+    }
+}
