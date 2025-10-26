180809b45ea1132cd30b69c9d64f68d26ad9effd
Amadeusz Kosik <git.stunning853@passmail.com>
2025-10-26 12:12:26 +0100

    Add solution: 0602 - SQL.

---
+++ 0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,14 @@
+SELECT 
+    id, 
+    COUNT(*) AS num
+FROM (
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id FROM RequestAccepted
+) requests
+GROUP BY
+    id
+ORDER BY
+    COUNT(*) DESC
+LIMIT 
+    1
