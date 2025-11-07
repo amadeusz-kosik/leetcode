6e43630c282b94fc7ebff68466c1551f1898840b
Amadeusz Kosik <git.stunning853@passmail.com>
2025-11-07 16:48:52 +0100

    Add solution: 1965 - SQL.

---
+++ 1965-employees-with-missing-information.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT
+    employee_id
+FROM (
+    SELECT * FROM Employees LEFT JOIN Salaries USING (employee_id) 
+        UNION
+    SELECT * FROM Employees RIGHT JOIN Salaries USING (employee_id)
+) emp
+WHERE
+    salary IS NULL OR name IS NULL
+ORDER BY
+    employee_id ASC
