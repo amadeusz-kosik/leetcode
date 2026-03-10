7e599f22e28d9d39ec90bbf879243938644a8f36
Amadeusz Kosik <tappers_teams.5d@icloud.com>
2026-03-10 10:17:30 +0100

    Add solution: 1486 - Rust.

---
+++ Rust/1486-xor-operation-in-an-array.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn xor_operation(n: i32, start: i32) -> i32 {
+        let mut result: i32 = 0;
+
+        for i in 0..n {
+            result ^= (start + 2 * i);
+        }
+
+        return result;
+    }
+}
