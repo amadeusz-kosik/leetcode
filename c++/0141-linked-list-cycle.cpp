4f1446f410c0cdb0c33d5f97916f1b458275394c
Amadeusz Kosik <commit.impale258@passmail.com>
2026-06-14 11:15:45 +0200

    Add solution: 0141 - C++.

---
+++ c++/0141-linked-list-cycle.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == nullptr) { return false; }
+
+        ListNode *hare = head;
+        ListNode *tortoise = head;
+
+        while(hare != nullptr && hare->next != nullptr) {
+            hare = hare->next;
+            hare = hare->next;
+            tortoise = tortoise->next;
+
+            if(hare == tortoise) { return true; }
+        }
+
+        return false;
+    }
+};
