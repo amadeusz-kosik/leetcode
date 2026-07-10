d7c87dfd3b9d6902b304e677468450c15e6f154e
Amadeusz Kosik <commit.impale258@passmail.com>
2026-07-10 20:30:00 +0200

    Add solution: 0724 - C++.

---
+++ dev-c++/0724-find-pivot-index.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        for(int i = 1; i < nums.size(); i += 1) {
+            nums[i] += nums[i - 1];
+        }
+
+        if(nums[nums.size() - 1] - nums[0] == 0) { 
+            return 0; 
+        }
+        
+        for(int i = 1; i < nums.size() - 1; i += 1) {
+            if(nums[i - 1] == (nums[nums.size() - 1] - nums[i])) { 
+                return i; 
+            }
+        }
+
+        if(nums[nums.size() - 2] == 0) { 
+            return nums.size() - 1; 
+        }
+
+        return -1;
+    }
+};
