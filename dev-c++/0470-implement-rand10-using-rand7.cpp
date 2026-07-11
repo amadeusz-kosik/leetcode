af9ebb0d042ac2e271711d35a477c26a4d19294c
Amadeusz Kosik <commit.impale258@passmail.com>
2026-07-11 13:30:15 +0200

    Add solution: 0470 - C++.

---
+++ dev-c++/0470-implement-rand10-using-rand7.cpp
@@ -0,0 +1,18 @@
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+class Solution {
+public:
+    int rand10() {
+        int result;
+
+        while(true) {
+            result = (rand7() - 1) * 7 + rand7();
+
+            if(result <= 40) {
+                return result % 10 + 1;
+            }
+        }
+    }
+};
