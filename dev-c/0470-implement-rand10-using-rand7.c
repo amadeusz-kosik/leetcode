d84e24dd323b4cd513c1996f8a6783d85df05c97
Amadeusz Kosik <commit.impale258@passmail.com>
2026-07-11 13:33:32 +0200

    Add solution: 0470 - C.

---
+++ dev-c/0470-implement-rand10-using-rand7.c
@@ -0,0 +1,15 @@
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+int rand10() {
+    int result;
+
+    while(true) {
+        result = (rand7() - 1) * 7 + rand7();
+
+        if(result <= 40) {
+            return result % 10 + 1;
+        }
+    }
+}
